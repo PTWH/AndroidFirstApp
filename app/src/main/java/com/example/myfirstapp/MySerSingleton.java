b719aac38094ee97a2163afe2390c0a69f743ce5
pt1 <touret.pierre@gmail.com>
2019-05-28 18:09:28 +0200

    Un hop la mise à jour du Vendredi soir
    # Conflicts:
    #	app/src/main/java/com/example/myfirstapp/MySerSingleton.java

--- app/src/main/java/com/example/myfirstapp/MySerSingleton.java
+++ app/src/main/java/com/example/myfirstapp/MySerSingleton.java
@@ -2,19 +2,34 @@ package com.example.myfirstapp;
 
 import android.content.Context;
 import android.provider.Settings;
+import com.example.myfirstapp.Room.AppDatabase;
+import com.example.myfirstapp.Room.FactoryRoom;
+import com.example.myfirstapp.Room.JSONData;
+import com.example.myfirstapp.Room.JSONDataDao;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MySerSingleton implements IAccess {
     private static MySerSingleton instance;
-    private Context appCtx;
+    private Context ctx;
     private SimpleDateFormat sdf;
-    private IAccess caller;
+    private MyHttpSingleton httpInstance;
+    private AppDatabase db;
+    private ExecutorService executorService;
+    private Future future;
 
     private MySerSingleton(Context context) {
-        appCtx = context;
+        executorService = Executors.newSingleThreadExecutor();
+        ctx = context;
+        db = FactoryRoom.getInstance(ctx);
         sdf = new SimpleDateFormat(" - yyyy/MM/dd HH:mm:ss");
     }
 
@@ -23,47 +38,83 @@ public class MySerSingleton implements IAccess {
         return instance;
     }
 
-    public void doHttpRequestSecure( IAccess demandeur , String theUrl,  Map<String, String> params ) {
-        caller = demandeur;
-        String Unique =  Settings.Secure.getString(appCtx.getContentResolver(), Settings.Secure.ANDROID_ID) + sdf.format(new Date());
+    public void doTheSerRequestWithJSON( String theUrl,  Map<String, String> params ) {
+        String Unique =  Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.ANDROID_ID) + sdf.format(new Date());
         params.put("uniqueid", Unique);
         JSONObject parameters = new JSONObject(params);
 
-        MyRoomSingleton roomInstance = MyRoomSingleton.getInstance(appCtx);
-        roomInstance.writeThePostHttp(Unique,parameters,theUrl);
 
-        MyHttpSingleton httpInstance = MyHttpSingleton.getInstance(appCtx);
-        httpInstance.doThePostHttp( this, theUrl,parameters );
+        final JSONData jsd = new JSONData();
+        jsd.uniqueid = Unique;
+        jsd.JSONStr = parameters.toString();
+        jsd.JSONUrl = theUrl;
+        future = executorService.submit(new Callable(){
+            @Override
+            public Object call() throws Exception {
+                JSONDataDao jsonDataDao;
+                jsonDataDao = db.jsonDataDao();
+                jsonDataDao.insert(jsd);
+                return jsd;
+            }
+        });
+
+        MyHttpSingleton httpInstance = MyHttpSingleton.getInstance(ctx);
+        httpInstance.doTheHttpRequestWithJSON( this, jsd.JSONUrl,parameters );
     }
 
     @Override
     public void success(JSONObject response) {
-        MyRoomSingleton roomInstance = MyRoomSingleton.getInstance(appCtx);
+        JSONData jsd =null;
         try
         {
-            roomInstance.waitForFutureAndDelete();
+            jsd = (JSONData) future.get();
         }
-        catch( Exception e ) {
+        catch( Exception e) {
             error(e.getMessage());
             return;
         }
-        caller.success(response);
+
+        final JSONData finalJsd = jsd;
+        future = executorService.submit(new Callable(){
+            @Override
+            public Object call() throws Exception {
+                JSONDataDao jsonDataDao;
+                jsonDataDao = db.jsonDataDao();
+                jsonDataDao.delete(finalJsd);
+                return null;
+            }
+        });
+
+        ((IAccess) ctx).success(response);
     }
 
     @Override
     public void error(String error) {
-        caller.error(error );
+        ((IAccess) ctx).error(error );
     }
 
-    public JSONObject showAll( IAccess demandeur ) {
-        caller = demandeur;
-        try {
-            MyRoomSingleton roomInstance = MyRoomSingleton.getInstance(appCtx);
-            demandeur.success(roomInstance.getAbortedHttpRequest());
+    public void showAll() {
+        future = executorService.submit(new Callable(){
+            @Override
+            public Object call() throws Exception {
+                JSONDataDao jsonDataDao = db.jsonDataDao();
+                return jsonDataDao.getAll();
+            }
+        });
+
+        try
+        {
+            List<JSONData> lst = (List<JSONData>) future.get();
+            JSONObject jso = new JSONObject();
+            JSONArray jsa = new JSONArray(lst);
+            for (int index = 0; index < lst.size(); index++) {
+                JSONData jsd = lst.get(index);
+                JSONObject jso2 = new JSONObject(jsd.JSONStr);
+                jsa.put(index, jso2);
+            }
+            jso.put("tab", jsa);
+            ((IAccess) ctx).success(jso);
         }
-        catch( Exception e ) {
-            error(e.getMessage());
-        }
-        return null;
+        catch( Exception e ) {((IAccess) ctx).error(e.getMessage());}
     }
 }
